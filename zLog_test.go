51b37148da0b8d4fe09835f477ae417eb55e691e
qiu dafa <qiudf@qnzsai.com>
2024-10-23 20:31:49 +0800

    test(zLog): 更新日志初始化参数并添加默认日志测试- 在 TestInit 函数中添加了更多日志配置参数，包括信息日志、错误日志、日志切割和时间格式 - 新增 TestDefaultLog 函数，用于测试 DefaultLog 的各种日志级别输出

--- zLog_test.go
+++ zLog_test.go
@@ -6,7 +6,12 @@ import (
 
 func TestInit(t *testing.T) {
 
-	err := Init(WithCommonLog("logs", "common.log", true))
+	err := Init(
+		WithCommonLog("logs", "common.log", true),
+		WithInfoLog("logs", "info.log", false),
+		WithErrorLog("logs", "error.log", false),
+		WithLogCut(1, 5, 30, true),
+		WithTimeFormat("2006-01-02 15:04:05.000"))
 	if err != nil {
 		t.Error(err)
 		return
@@ -15,4 +20,11 @@ func TestInit(t *testing.T) {
 	Log.Error("hello world")
 	Log.Debug("hello world")
 	Log.Warn("hello world")
+}
+
+func TestDefaultLog(t *testing.T) {
+	DefaultLog.Info("hello world")
+	DefaultLog.Error("hello world")
+	DefaultLog.Debug("hello world")
+	DefaultLog.Warn("hello world")
 }
