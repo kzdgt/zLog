7c13dbf3082a8191de5eca6efcf65e236e071a3a
qiu dafa <qiudf@qnzsai.com>
2025-01-21 16:54:16 +0800

    feat(zLog): 添加 GormLogger 实现
    实现了 GormLogger 结构体和相关方法，用于在 GORM 中集成 zap 日志库。
    主要功能包括：
    - 创建 GormLogger 类型，包含 zap.Logger
    - 实现 New 函数以创建 GormLogger 实例
    - 实现 Print 方法处理不同类型的日志输出，包括 SQL 日志和其他日志
    - 在 SQL 日志中记录查询语句、参数、执行时间、受影响行数等信息
    - 其他日志中记录源信息和其他相关数据

---
+++ gormZLog.go
@@ -0,0 +1,36 @@
+package zLog
+
+import (
+	"go.uber.org/zap"
+	"time"
+)
+
+type GormLogger struct {
+	zap *zap.Logger
+}
+
+func New(logger *zap.Logger) GormLogger {
+	return GormLogger{zap: logger}
+}
+
+func (l GormLogger) Print(values ...interface{}) {
+	if len(values) < 2 {
+		return
+	}
+
+	switch values[0] {
+	case "sql":
+		l.zap.Debug("gorm.debug.sql",
+			zap.String("query", values[3].(string)),
+			zap.Any("values", values[4]),
+			zap.Float64("duration in ms", float64(values[2].(time.Duration))/float64(time.Millisecond)),
+			zap.Int64("affected-rows", values[5].(int64)),
+			zap.String("source", values[1].(string)), // if AddCallerSkip(6) is well defined, we can safely remove this field
+		)
+	default:
+		l.zap.Debug("gorm.debug.other",
+			zap.Any("values", values[2:]),
+			zap.String("source", values[1].(string)), // if AddCallerSkip(6) is well defined, we can safely remove this field
+		)
+	}
+}
